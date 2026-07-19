0b2fa46fe44337b2ed69ec27cdc35d25171bbe94
shaiju <shaijuvr@rediffmail.com>
2026-07-19 18:02:29 +0530

    fullly-functional-vectorized-RAG based assistant.py

--- week5/3-rag-using-vector-db.py
+++ week5/3-rag-using-vector-db.py
@@ -5,10 +5,12 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader,TextLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 import numpy as np
 from sklearn.manifold import TSNE
 import plotly.graph_objects as go
-
+import gradio as gr
 
 MODEL="gpt-4.1-mini"
 DB_NAME="kb_vectordb"
@@ -107,4 +109,25 @@ def print_vector_3DView():
 
     fig.show()
 
-print_vector_3DView()
+def chat(message,history):
+    vectorstore=Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+    retriever=vectorstore.as_retriever()
+    docs=retriever.invoke(message)
+    kb="\n\n".join(doc.page_content for doc in docs)
+    system_prompt=f"""
+    You are a helpful assistant representing the company Insurellm with a knowledgebase
+    Insurellm's users will be interacting with you
+    If the information is not available in KB, just reply with statement "Sorry,I don't know about it". 
+    If the information is relevant to the domain, you will help them with details
+    KB: {kb}
+    """
+    messages=[
+        SystemMessage(system_prompt),
+        HumanMessage(message)
+    ]
+    llm=ChatOpenAI(model=MODEL, temperature=0)
+    response=llm.invoke(messages)
+    return response.content
+
+gr.ChatInterface(fn=chat).launch(inbrowser=True)
+    
