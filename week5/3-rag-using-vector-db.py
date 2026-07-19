17661af373450e72d696788cca575810c80ddf1c
shaiju <shaijuvr@rediffmail.com>
2026-07-19 12:33:24 +0530

    vectordb created

--- week5/3-rag-using-vector-db.py
+++ week5/3-rag-using-vector-db.py
@@ -1,4 +1,4 @@
-import glob
+import glob, shutil
 from os import path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader,TextLoader
@@ -27,16 +27,13 @@ def split_documents(documents:[]):
     return chunks
 
 chunks=split_documents(get_documents())
-print(len(chunks))
 
-'''
 def create_kb_vector_db():
     chunks=split_documents(get_documents())
     embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     if(path.exists(DB_NAME)):
-        Chroma(persist_directory=DB_NAME,embedding_function=embeddings).delete_collection()
+        shutil.rmtree(DB_NAME)
     vectorstore=Chroma.from_documents(documents=chunks,persist_directory=DB_NAME, embedding=embeddings)
     print(f"vectorstore created with {vectorstore._collection.count()} documents")
 
 create_kb_vector_db()
-'''
