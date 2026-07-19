eba4eb05d35a0a897ec64d2b5138bfbc9735dfc1
shaiju <shaijuvr@rediffmail.com>
2026-07-19 17:05:13 +0530

    vectordb visualization ready

--- week5/3-rag-using-vector-db.py
+++ week5/3-rag-using-vector-db.py
@@ -1,13 +1,18 @@
 import glob, shutil
 from os import path
+from langchain_core import documents
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader,TextLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
+import numpy as np
+from sklearn.manifold import TSNE
+import plotly.graph_objects as go
 
 
 MODEL="gpt-4.1-mini"
 DB_NAME="kb_vectordb"
+VECTORIZING_MODEL="all-MiniLM-L6-v2"
 
 def get_documents():
     documents=[]
@@ -26,14 +31,80 @@ def split_documents(documents:[]):
     chunks=text_splitter.split_documents(documents)
     return chunks
 
-chunks=split_documents(get_documents())
+embeddings=HuggingFaceEmbeddings(model_name=VECTORIZING_MODEL)
 
 def create_kb_vector_db():
     chunks=split_documents(get_documents())
-    embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     if(path.exists(DB_NAME)):
         shutil.rmtree(DB_NAME)
     vectorstore=Chroma.from_documents(documents=chunks,persist_directory=DB_NAME, embedding=embeddings)
     print(f"vectorstore created with {vectorstore._collection.count()} documents")
 
-create_kb_vector_db()
+def get_vector_dimensions_info():
+    vectorstore=Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+    result=vectorstore.get(include=['embeddings'])
+    print(f'vectors are of {len(result['embeddings'][0])} dimensions')
+
+
+def print_vector_2DView():
+    vectorstore=Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+    result=vectorstore.get(include=['embeddings','documents','metadatas'])
+    vectors=np.array(result['embeddings'])
+    documents=result['documents']
+    metadatas=result['metadatas']
+    doc_types=[metadata['doc_type'] for metadata in metadatas ]
+    colors=[['blue','green','red','orange'][['employees','contracts','products','company'].index(doc_type)]for doc_type in doc_types]
+    tsne = TSNE(n_components=2, random_state=42)
+    reduced_vectors = tsne.fit_transform(vectors)
+
+    # Create the 2D scatter plot
+    fig = go.Figure(data=[go.Scatter(
+        x=reduced_vectors[:, 0],
+        y=reduced_vectors[:, 1],
+        mode='markers',
+        marker=dict(size=5, color=colors, opacity=0.8),
+        text=[f"Type: {t}<br>Text: {d[:100]}..." for t, d in zip(doc_types, documents)],
+        hoverinfo='text'
+    )])
+
+    fig.update_layout(title='2D Chroma Vector Store Visualization',
+        scene=dict(xaxis_title='x',yaxis_title='y'),
+        width=800,
+        height=600,
+        margin=dict(r=20, b=10, l=10, t=40)
+    )
+
+    fig.show()
+
+def print_vector_3DView():
+    vectorstore=Chroma(persist_directory=DB_NAME, embedding_function=embeddings)
+    result=vectorstore.get(include=['embeddings','documents','metadatas'])
+    vectors=np.array(result['embeddings'])
+    documents=result['documents']
+    metadatas=result['metadatas']
+    doc_types=[metadata['doc_type'] for metadata in metadatas ]
+    colors=[['blue','green','red','orange'][['employees','contracts','products','company'].index(doc_type)]for doc_type in doc_types]
+    tsne = TSNE(n_components=3, random_state=42)
+    reduced_vectors = tsne.fit_transform(vectors)
+
+    # Create the 3D scatter plot
+    fig = go.Figure(data=[go.Scatter3d(
+        x=reduced_vectors[:, 0],
+        y=reduced_vectors[:, 1],
+        z=reduced_vectors[:, 2],
+        mode='markers',
+        marker=dict(size=5, color=colors, opacity=0.8),
+        text=[f"Type: {t}<br>Text: {d[:100]}..." for t, d in zip(doc_types, documents)],
+        hoverinfo='text'
+    )])
+
+    fig.update_layout(title='3D Chroma Vector Store Visualization',
+        scene=dict(xaxis_title='x',yaxis_title='y',zaxis_title='z'),
+        width=900,
+        height=700,
+        margin=dict(r=20, b=10, l=10, t=40)
+    )
+
+    fig.show()
+
+print_vector_3DView()
